6ba5e89a542e585c36bbd31f486d681f6338606c
Dmitry Baranov <dimch2205@gmail.com>
2023-08-08 17:19:24 +0300

    Styles added

--- js/background.js
+++ js/background.js
@@ -2,19 +2,20 @@ const urls = {urls:["<all_urls>"]};
 
 chrome.runtime.onMessage.addListener((...args)=>{
     console.log(args[0]);
-    const {state} = args[0];
+    const state = args[0];
     let timerID = null;
 
     const reqListener = (reqType,reqData) => {
-            const context = this;
             clearInterval(timerID);
             timerID = setTimeout(()=>{
-                console.log(`Req ${reqType} sended from context ${context}:`, reqData);
+                // console.log(`Req ${reqType} sended:initiator:${reqData.initiator}, url: ${reqData.url}, type:${reqData.type}`);
+                chrome.runtime.sendMessage({...reqData,reqType});
             },500);
+            
     };
 
     if(state === "is_active"){
-        chrome.webRequest.onBeforeRequest.addListener((req)=>reqListener("before options",req),urls);
+        chrome.webRequest.onBeforeRequest.addListener((req)=>reqListener("reqOptions",req),urls);
         chrome.webRequest.onBeforeSendHeaders.addListener((req)=>reqListener("headers",req),urls);
     }else{
         chrome.webRequest.onBeforeRequest?.removeListener((req)=>reqListener("",req),urls);
--- js/popup.js
+++ js/popup.js
@@ -7,36 +7,52 @@ class Sentinel {
             this.initExtension = this.initExtension.bind(this);
             this.getComponent = this.getComponent.bind(this);
             // this.requestCallback = this.requestCallback.bind(this);
-            this.state = localStorage.getItem("sentinel_state");
-            this.receiveBackgroundMessage = this.receiveBackgroundMessage.bind(this);
+            this.state;
         }
 
         
         
         async initExtension(){
+            this.state = await localStorage.getItem("sentinel_state");
+            this.runtime.onMessage.addListener((msg)=>this.#receiveBackgroundMessage(msg));
             document.body.querySelector(".guard_status").appendChild(document.createElement("custom-switch"));
             this.switch = document.getElementsByTagName("custom-switch")[0];
-            // const switchThumb = this.switch.shadow.childNodes[3];
             
             this.switch.setAttribute("state",this.state);
-            // switchThumb.setAttribute("state",this.state);
             this.runtime.sendMessage(this.state);
             
             this.switch.addEventListener("click",(e) => {
                 const newState = this.state === "is_not_active" ? "is_active" : "is_not_active";
                 this.state = newState;
                 this.switch.setAttribute("state", this.state);
-                // switchThumb.setAttribute("state", this.state);
                 localStorage.setItem("sentinel_state",this.state);
-                this.runtime.sendMessage({
-                    state:this.state,
-                });
+                this.runtime.sendMessage({state:this.state});
             });
         };
         
-        receiveBackgroundMessage(msg){
-            console.log("MSG FROM POPUP:",msg)
+        #receiveBackgroundMessage(reqData){
+            console.log("MSG FROM POPUP:",reqData)
+            this.#addToList(reqData);
         }
+        
+        #addToList(reqData){
+            const stopList = ["tabId","frameId","frameType","parentFrameId","requestId","documentLifecycle"];
+            let list = document.querySelector("#requests_list");
+            console.log(list);
+            const listItem = document.createElement("li");
+            for(let pair of Object.entries(reqData)){
+                let [pairKey,pairValue] = pair;
+                if(!stopList.includes(pairKey)){
+                    if(pairKey === "timeStamp") pairValue = new Date(pairValue);
+                    const span = document.createElement("span");
+                    span.innerText = `${pairKey}: ${pairValue}`;
+                    listItem.appendChild(span);
+                }
+            };
+            list.appendChild(listItem);
+            // list.scrollTop = list.scrollHeight;
+        }
+
         
         async getComponent(path,elId = null){
             const response = await fetch(path);
@@ -59,3 +75,4 @@ class Sentinel {
 
     window.$MetricsSentinel = new Sentinel(runtime);
     window.$MetricsSentinel.initExtension();
+    window.onbeforeunload = runtime.onMessage.removeListener((msg)=> Sentinel.receiveBackgroundMessage(msg));
